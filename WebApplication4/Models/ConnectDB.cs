fc26a21174d9d8b2c1e21f4dc215ba6566aa22ab
a0979470582 <45554149+a0979470582@users.noreply.github.com>
2019-05-22 21:02:35 +0800

    Update ConnectDB.cs

--- WebApplication4/Models/ConnectDB.cs
+++ WebApplication4/Models/ConnectDB.cs
@@ -6,7 +6,7 @@ using System.Web;
 namespace WebApplication4.Models
 {
     public class ConnectDB
-    {
+    {//please change the db_file's path
         public static string GetDBPath()
         {
             return "C:/Users/a0979/source/repos/WebApplication4/WebApplication4/App_Data/bookdata.bin";
